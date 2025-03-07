f885aea520a444a7ee314e9e8abc394ad2b3479a
As1ary <kingpetch01@gmail.com>
2025-03-08 01:08:50 +0700

    Add Drag Character

--- Assets/Scripts/Command/LeftClick.cs
+++ Assets/Scripts/Command/LeftClick.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class LeftClick : MonoBehaviour
 {
@@ -7,12 +8,18 @@ public class LeftClick : MonoBehaviour
     private Camera cam;
     [SerializeField]
     private LayerMask layerMask; 
+    
+    [SerializeField]
+    private RectTransform boxSelection;
+    private Vector2 oldAnchoredPos;
+    private Vector2 startPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         instance = this;
         cam = Camera.main;
         layerMask = LayerMask.GetMask("Ground","Character","Building","Item");
+        boxSelection = UIManager.instance.SelectionBox;
     }
 
     private void SelectCharactor(RaycastHit hit)
@@ -51,13 +58,57 @@ public class LeftClick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            startPos = Input.mousePosition;
+            
+            if (EventSystem.current.IsPointerOverGameObject())
+                return;
             ClearEverything();
         }
-        if(Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButton(0))
         {
+            UpdateSelectionBox(Input.mousePosition);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            ReleaseSelectionBox(Input.mousePosition);
             TrySelect(Input.mousePosition);
         }
+    }
+    private void UpdateSelectionBox (Vector2 mousePos) 
+    {
+        if(!boxSelection.gameObject.activeInHierarchy)
+            boxSelection.gameObject.SetActive(true);
+        float width = mousePos.x - startPos.x;
+        float height = mousePos.y - startPos.y;
+        boxSelection.anchoredPosition = startPos + new Vector2(width / 2, height / 2);
+        
+        width = Mathf.Abs(width);
+        height = Mathf.Abs(height);
+        boxSelection.sizeDelta = new Vector2(width, height);
+    
+        oldAnchoredPos = boxSelection.anchoredPosition;    
+    }
+    private void ReleaseSelectionBox(Vector2 mousePos)
+    {
+        Vector2 corner1; 
+        Vector2 corner2; 
+        boxSelection.gameObject.SetActive(false);
+        corner1 = oldAnchoredPos - (boxSelection.sizeDelta / 2);
+        corner2 = oldAnchoredPos + (boxSelection.sizeDelta / 2);
+
+        foreach (Character member in PartyManager.instance.Members)
+        {
+            Vector2 unitPos = cam.WorldToScreenPoint(member.transform.position);
+            if ((unitPos.x > corner1.x && unitPos.x < corner2.x) 
+                && (unitPos.y > corner1.y && unitPos.y < corner2.y))
+            {
+                PartyManager.instance.SelectChars.Add(member);
+                member.ToggleRingSelection(true);
+            }
+        }
+
+        boxSelection.sizeDelta = new Vector2(0, 0);
     }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+     [SerializeField]
+    private RectTransform selectionBox;
+    public RectTransform SelectionBox { get { return selectionBox; } }
+
+    public static UIManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
