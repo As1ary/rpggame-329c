0a97109b1c8785daefceab1b6db6fa797546d856
As1ary <kingpetch01@gmail.com>
2025-03-07 10:45:56 +0700

    Add move player character

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -15,10 +15,13 @@ public abstract class Character : MonoBehaviour
    protected NavMeshAgent navAgent;
 
    protected Animator anim;
-   public Animator Anim;
+   public Animator Anim{get{ return anim;}}
 
    [SerializeField] protected CharState state;
    public CharState State { get { return state; } }
+   /*[SerializeField] 
+   protected GameObject ringSelection;
+   public GameObject RingSelection { get { return ringSelection;}}*/
 
    void Awake()
    {
@@ -53,5 +56,9 @@ public abstract class Character : MonoBehaviour
      {
           SetState(CharState.Idle);
      }
+   }
+   public void ToggleRingSelection(bool flag)
+   {
+     //ringSelection.SetActive(flag);
    }
 }
