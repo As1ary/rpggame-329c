01c68ee9d6176f3983749c02f536899cd8cc8fc8
As1ary <kingpetch01@gmail.com>
2025-03-08 07:39:01 +0700

    Update UI Add Toggle Pause&Ai

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
      [SerializeField]
     private RectTransform selectionBox;
     public RectTransform SelectionBox { get { return selectionBox; } }
+
+    [SerializeField]
+    private Toggle togglePauseUnpause;
 
     public static UIManager instance;
 
@@ -21,6 +25,34 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            togglePauseUnpause.isOn = !togglePauseUnpause.isOn;
+        }
+    }
+    public void ToggleAI(bool isOn)
+    {
+        foreach (Character member in PartyManager.instance.Members)
+        {
+            AttackAI ai = member.gameObject.GetComponent<AttackAI>();
+
+            if (ai != null)
+                ai.enabled = isOn;
+        }
+    }
+    public void SelectAll()
+    {
+        foreach(Character member in PartyManager.instance.Members)
+        {
+            if(member.CurHP > 0)
+            {
+                member.ToggleRingSelection(true);
+                PartyManager.instance.SelectChars.Add(member);
+            }
+        }
+    }
+    public void PauseUnpause (bool isOn) 
+    {
+        Time.timeScale = isOn ? 0 : 1;    
     }
 }
