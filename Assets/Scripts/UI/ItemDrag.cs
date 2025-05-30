c03d8faa45797fa7566dd274445a20305d49aa6b
As1ary <kingpetch01@gmail.com>
2025-05-30 23:04:07 +0700

    DragItemInUISuccess

---
+++ Assets/Scripts/UI/ItemDrag.cs
@@ -0,0 +1,49 @@
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ItemDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    private Item item;
+    public Item Item { get { return item; } set { item = value; } }
+
+    [SerializeField]
+    public Transform iconParent;
+    public Transform IconParent { get { return iconParent; } set { iconParent = value; } }
+
+    [SerializeField]
+    private Image image;
+    public Image Image { get { return image; } set { image = value; } }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("BeginDrag");
+        iconParent = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        transform.position = Input.mousePosition;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("EndDrag");
+        transform.SetParent(iconParent);
+        image.raycastTarget = true;
+    }
+}
